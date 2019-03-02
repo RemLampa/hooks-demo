3073a169934b007f8ab60682c8a978eba8ec0503
Rem Lampa <rem@remlampa.com>
2019-03-02 09:48:34 +0800

    Updates

---
+++ precache-manifest.c3394e67f3e0906e9a7ae17635e90f51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8050cf604fc9b2b60f5e",
+    "url": "/static/js/main.8050cf60.chunk.js"
+  },
+  {
+    "revision": "f521c42c92c31ef06ea7",
+    "url": "/static/js/2.f521c42c.chunk.js"
+  },
+  {
+    "revision": "8050cf604fc9b2b60f5e",
+    "url": "/static/css/main.a7672e67.chunk.css"
+  },
+  {
+    "revision": "a4bc5ef63c58c43ce2bc8b1084305662",
+    "url": "/index.html"
+  }
+];
